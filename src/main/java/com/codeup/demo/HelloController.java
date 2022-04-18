85730ba5d92fc03b8ad2a1f8734133da3d57db71
Angel Santana <angel.d.santana00@gmail.com>
2022-04-18 14:16:03 -0500

    pushin p

---
+++ src/main/java/com/codeup/demo/HelloController.java
@@ -0,0 +1,25 @@
+package com.codeup.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello(){
+
+        return "Hello there from the Sirius cohort";
+
+    }
+
+    @RequestMapping(path= "/hello/{name}/and/{age}", method = RequestMethod.GET)
+    @ResponseBody
+    public String helloNameAge(@PathVariable String name, @PathVariable int age){
+
+        return "Wassup! Thanks for letting me know your name is " + name + ", You also told me you were " + age + " years old";
+
+    }
+
+}
