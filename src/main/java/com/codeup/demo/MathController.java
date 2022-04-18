06638d2725ceb6b47ebefea9b7f3ea7b41a7a3af
Angel Santana <angel.d.santana00@gmail.com>
2022-04-18 15:25:38 -0500

    pushin p 3/ math controller done

---
+++ src/main/java/com/codeup/demo/MathController.java
@@ -0,0 +1,35 @@
+package com.codeup.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MathController {
+
+        @GetMapping("/add/{x}/and/{y}")
+    @ResponseBody
+    public int sum(@PathVariable int x, @PathVariable int y) {
+        return x + y;
+    }
+
+    @GetMapping("/subtract/{x}/from/{y}")
+    @ResponseBody
+    public int difference(@PathVariable int x, @PathVariable int y) {
+        return x - y;
+    }
+
+    @GetMapping("/multiply/{x}/and/{y}")
+    @ResponseBody
+    public int product(@PathVariable int x, @PathVariable int y) {
+        return x * y;
+    }
+
+    @GetMapping("/divide/{x}/by/{y}")
+    @ResponseBody
+    public int quotient(@PathVariable int x, @PathVariable int y) {
+        return x / y;
+    }
+
+}
