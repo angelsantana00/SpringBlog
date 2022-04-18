0f4145892abf295dd6da721d4351e8d962d2b5cc
Angel Santana <angel.d.santana00@gmail.com>
2022-04-18 14:19:44 -0500

    pushin p 2

--- src/main/java/com/codeup/demo/HomeController.java
+++ src/main/java/com/codeup/demo/HomeController.java
@@ -1,4 +1,16 @@
 package com.codeup.demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/landing")
+    @ResponseBody
+    public String land() {
+
+        return "THIS IS THE LANDING PAGE";
+
+    }
 }
