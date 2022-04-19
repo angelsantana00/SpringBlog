bf03fdc36fd8a24835f6cf16bba112aaa526180e
Angel Santana <angel.d.santana00@gmail.com>
2022-04-19 12:25:02 -0500

    dice rolla

--- src/main/java/com/codeup/demo/RollDiceController.java
+++ src/main/java/com/codeup/demo/RollDiceController.java
@@ -1,4 +1,32 @@
 package com.codeup.demo;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Random;
+
+@Controller
 public class RollDiceController {
+
+    @GetMapping("/rollDice")
+    public String rollDice() {
+        return "rollDice";
+    }
+
+    @GetMapping("/rollDice/{n}")
+    public String rollDiceGuess(@PathVariable int n, Model model) {
+
+        Random rand = new Random();
+        int random = rand.nextInt((6 - 1) + 1) + 1;
+
+        model.addAttribute("diceRoll", random);
+        model.addAttribute("userGuess", n);
+        model.addAttribute("isCorrect", random == n);
+
+        return "rollDice";
+
+    }
 }
