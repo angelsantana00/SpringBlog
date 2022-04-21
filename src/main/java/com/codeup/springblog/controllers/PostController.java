99ebfcf7213789da5c7a089e58449d99b77f3de6
Angel Santana <angel.d.santana00@gmail.com>
2022-04-21 10:13:27 -0500

    created post htmls

---
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -0,0 +1,90 @@
+
+
+package com.codeup.springblog.controllers;
+import com.codeup.springblog.models.Post;
+import com.codeup.springblog.repos.PostRepository;
+import com.codeup.springblog.repos.UserRepository;
+import com.codeup.springblog.services.EmailService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+public class PostController<PostRepository, UserRepository, EmailService> {
+
+    private final PostRepository postDao;
+    private final UserRepository userDao;
+    private final EmailService emailService;
+
+    public PostController(PostRepository postDao, UserRepository userDao, EmailService emailService) {
+        this.postDao = postDao;
+        this.userDao = userDao;
+        this.emailService = emailService;
+    }
+
+    @GetMapping("/posts")
+    public String showPosts(Model model) {
+        List<Post> allPosts = postDao.findAll();
+        model.addAttribute("posts", allPosts);
+        return "post/index";
+    }
+
+    @GetMapping("/posts/{id}")
+    public String showOnePost(@PathVariable long id, Model model) {
+        Post post = postDao.getById(id);
+        model.addAttribute("postId", id);
+        model.addAttribute("post", post);
+        return "post/show";
+    }
+
+    @GetMapping("/posts/create")
+    public String showCreatePostForm(Model model) {
+        model.addAttribute("post", new Post());
+        return "post/create";
+    }
+
+    @PostMapping("/posts/create")
+    public String createPost(@ModelAttribute Post postToAdd) {
+
+        postToAdd.setUser(userDao.getById(1L));
+        emailService.prepareAndSend(
+                postToAdd,
+                "New Post",
+                "You created a Post"
+        );
+        postDao.save(postToAdd);
+        return "redirect:/posts";
+    }
+
+    @GetMapping("/posts/edit/{id}")
+    public String showEditPostForm(@PathVariable long id, Model model) {
+        Post postToEdit = postDao.getById(id);
+        model.addAttribute("postToEdit",postToEdit);
+        return "post/edit";
+    }
+
+    @PostMapping("/posts/edit/{id}")
+    public String editPost(
+            @PathVariable long id,
+            @ModelAttribute Post updatedPost
+    ) {
+        updatedPost.setId(id);
+        updatedPost.setUser(userDao.getById(1L));
+        postDao.save(updatedPost);
+
+        return "redirect:/posts";
+
+    }
+
+    @PostMapping("/posts/delete/{id}")
+    public String deletePost(@PathVariable long id) {
+        Post postToDelete = postDao.getById(id);
+        postDao.delete(postToDelete);
+
+        return "redirect:/posts";
+
+    }
+
+}
--- src/test/java/com/codeup/springblog/DemoApplicationTests.java
+++ src/test/java/com/codeup/springblog/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.codeup.demo;
+package com.codeup.springblog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
